ccc30dd7da2c0114fb552953923b2cf5a55d2e82
unofficialyaduvanshi <kk08161@gmail.com>
2026-05-21 19:20:47 +0530

    fix: changed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./styles/global.css"; // IMPORTANT: load your global styles
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/global.css"; // IMPORTANT: load your global styles
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const startApp = () => root.render(<App />);
+
+if ("requestIdleCallback" in window) {
+  requestIdleCallback(startApp);
+} else {
+  setTimeout(startApp, 1);
+}
